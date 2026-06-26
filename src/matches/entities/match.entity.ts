334552834ae990ac831ef0e51834d010a909724c
Raul Lize Teixeira <lizeraul1@gmail.com>
2026-06-26 20:18:00 -0300

    refactor(matches/entities): define MatchEntity
    replace empty Match class with fully defined MatchEntity, add all required fields and Prisma MatchStatus import

--- src/matches/entities/match.entity.ts
+++ src/matches/entities/match.entity.ts
@@ -1,1 +1,14 @@
-export class Match {}
+import { MatchStatus } from '@prisma/client';
+
+export class MatchEntity {
+  id: string;
+  date: Date;
+  location: string;
+  status: MatchStatus;
+  homeTeamId: string;
+  homeTeamScore: number | null;
+  awayTeamId: string;
+  awayTeamScore: number | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
